078db2d2966403a7995ace69236a41aff901c160
MartaKhatsiur <94527858+MartaKhatsiur@users.noreply.github.com>
2022-09-12 23:48:49 +0300

    Practice #1

---
+++ Pseudorandom.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class Ex1{
+    static void Main (string[] args)
+        {
+            var r = new Random(1000);
+            for (int i = 0; i < 5; i++)
+                Console.Write(r.Next(0,10) + " ");
+        }
+    }
---
+++ crypto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography;
+public class Ex2
+{
+    public static void Main(string[] args)
+    {
+        using (var r1 = new RNGCryptoServiceProvider())
+        {
+            var r2 = new byte[64];
+            r1.GetBytes(r2);
+            for (int i = 0; i < 5; i++)
+                Console.Write(r2[i]);
+
+        }
+    }
+
+}
